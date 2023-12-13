9f4807f9dcbb530844ced1633103fbdd4eda8637
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2023-12-13 17:10:21 +0000

    feat: add a types-in-jsdoc plugin
    This will replace the check-types plugin
    
    Release-As: 0.1.0

---
+++ plugins/types-in-jsdoc/index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const {Plugin} = require('@rmtc/plugin');
+
+class TypesInJsdoc extends Plugin {
+
+	/**
+	 * @type {import('@rmtc/plugin').ConfigMethod}
+	 */
+	configure(config) {
+
+		// Set some default configurations
+		config = Object.assign({
+			project: 'jsconfig.json'
+		}, config);
+
+		return config;
+	}
+
+	/**
+	 * @type {import('@rmtc/plugin').InitMethod}
+	 */
+	init() {
+		this.defineStep('type-in-jsdoc:check-types', this.checkTypes.bind(this));
+		this.defineStep('type-in-jsdoc:build-typedefs', this.buildTypeDefinitions.bind(this));
+		this.defineWorkflow('verify', ['type-in-jsdoc:check-types']);
+		this.defineWorkflow('build', ['type-in-jsdoc:build-typedefs']);
+	}
+
+	/**
+	 * @type {import('@rmtc/plugin').StepFunction}
+	 */
+	async checkTypes() {
+		await this.exec('tsc', [
+			'--noEmit',
+			'--project',
+			this.config.project
+		]);
+		this.log.info('no type errors found');
+	}
+
+	/**
+	 * @type {import('@rmtc/plugin').StepFunction}
+	 */
+	async buildTypeDefinitions() {
+		await this.exec('tsc', [
+			'--noEmit',
+			'false',
+			'--emitDeclarationOnly',
+			'--project',
+			this.config.project
+		]);
+		this.log.info('type definitions built successfully');
+	}
+
+}
+
+exports.Plugin = TypesInJsdoc;
